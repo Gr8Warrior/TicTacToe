05f664052bc8b5e4163b4da3754ca659c034c5d2
shailendrasuriyal <shailendra.suriyal@intimetec.com>
2017-01-10 12:08:34 +0530

    restructured images in the project

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -12,9 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Loaded ")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print("button pressed \(sender.tag)")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
