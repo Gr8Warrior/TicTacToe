244c4f0d190f692dea1f203202aa8f9c172f6cba
shailendrasuriyal <shailendra.suriyal@intimetec.com>
2017-01-10 12:18:14 +0530

    Applied Player's turn functionality

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,20 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("Loaded ")
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+// 1 for nought and 2 for cross
+    var activePlayer = 1 ;
 
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        print("button pressed \(sender.tag)")
-    }
+override func viewDidLoad() {
+    super.viewDidLoad()
+    print("Loaded ")
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@IBAction func buttonPressed(_ sender: UIButton) {
+    print("button pressed \(sender.tag)")
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    if activePlayer == 1{
+            sender.setImage(UIImage(named: "nought.png"), for: []);
+         activePlayer = 2;
+    } else {
+        sender.setImage(UIImage(named: "cross.png"), for: []);
+        activePlayer = 1;
     }
+}
+
+override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+}
 
 
 }
